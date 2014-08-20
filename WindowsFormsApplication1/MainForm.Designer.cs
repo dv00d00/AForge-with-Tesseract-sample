a47430f28a6e9e86b6b922fa71df76f30e74f5fc
dv00d00 <dmykush@gmail.com>
2014-08-20 15:00:14 +0300

    refactor

--- WindowsFormsApplication1/MainForm.Designer.cs
+++ WindowsFormsApplication1/MainForm.Designer.cs
@@ -28,49 +28,49 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.sourcePic = new System.Windows.Forms.PictureBox();
+            this.processedPic = new System.Windows.Forms.PictureBox();
+            this.recognizedText = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.sourcePic)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.processedPic)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(207, 90);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
+            this.sourcePic.Location = new System.Drawing.Point(12, 12);
+            this.sourcePic.Name = "sourcePic";
+            this.sourcePic.Size = new System.Drawing.Size(207, 90);
+            this.sourcePic.TabIndex = 0;
+            this.sourcePic.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(12, 108);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(207, 89);
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
+            this.processedPic.Location = new System.Drawing.Point(12, 108);
+            this.processedPic.Name = "processedPic";
+            this.processedPic.Size = new System.Drawing.Size(207, 89);
+            this.processedPic.TabIndex = 1;
+            this.processedPic.TabStop = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(25, 227);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(322, 20);
-            this.textBox1.TabIndex = 2;
+            this.recognizedText.Location = new System.Drawing.Point(25, 227);
+            this.recognizedText.Name = "recognizedText";
+            this.recognizedText.Size = new System.Drawing.Size(322, 20);
+            this.recognizedText.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(544, 443);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.recognizedText);
+            this.Controls.Add(this.processedPic);
+            this.Controls.Add(this.sourcePic);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.Load += new System.EventHandler(this.OnLoad);
+            ((System.ComponentModel.ISupportInitialize)(this.sourcePic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.processedPic)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -78,9 +78,9 @@
 
         #endregion
 
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox sourcePic;
+        private System.Windows.Forms.PictureBox processedPic;
+        private System.Windows.Forms.TextBox recognizedText;
     }
 }
 
--- WindowsFormsApplication1/MainForm.cs
+++ WindowsFormsApplication1/MainForm.cs
@@ -1,13 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using AForge;
 using AForge.Imaging.Filters;
 using Tesseract;
 
@@ -20,57 +12,31 @@ namespace WindowsFormsApplication1
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void OnLoad(object sender, EventArgs e)
         {
-            var path = @"img\rand.jpg";
+            const string path = @"img\rand.jpg";
             
             var bitmap = AForge.Imaging.Image.FromFile(path);
-            this.pictureBox1.Image = bitmap;
 
+            this.sourcePic.Image = bitmap;
 
             var image = AForge.Imaging.Image.Clone(bitmap, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-            var colorFiltering = new ColorFiltering();
-            int red1 = 50;
-            int red2 = 100;
-            int green1 = 50;
-            int green2 = 100;
-            int bluel = 50;
-            int blue2 = 100;
-            colorFiltering.Red = new IntRange(red1, red2);
-            colorFiltering.Green = new IntRange(green1, green2);
-            colorFiltering.Blue = new IntRange(bluel, blue2);
 
-            var simplePosterization = new SimplePosterization
-            {
-                PosterizationInterval = 100,
-                FillingType =  SimplePosterization.PosterizationFillingType.Min,
+            var threshold = new Threshold { ThresholdValue = 128 };
 
-            };
+            var greyscaled = Grayscale.CommonAlgorithms.RMY.Apply(image);
 
-            var threshold = new Threshold()
-            {
-                ThresholdValue = 128
-            };
-
-            var levelsLinear = new LevelsLinear();
-
-
-            var grayscale = Grayscale.CommonAlgorithms.RMY;
-            var greyscaled = grayscale.Apply(image);
             threshold.ApplyInPlace(greyscaled);
-            //Bitmap nImage2 = grayscale.Apply(nImage);
 
-            this.pictureBox2.Image = greyscaled;
+            this.processedPic.Image = greyscaled;
 
-            using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+            using (var engine = new TesseractEngine(@"./tessdata", "eng"))
             {
                 engine.SetVariable("tessedit_char_whitelist", "0123456789"); // If digit only
                 var process = engine.Process(greyscaled);
                 var text = process.GetText();
-                textBox1.Text = text;
+                recognizedText.Text = text;
             }
         }
-
-        
     }
 }
